ac1ec1c5659576c0c07d85b541857f72ed335bdf
Baryshnikov Nikolay <baryshnikov@rbc.ru>
2019-07-31 14:50:15 +0300

    Удалил вызов поиска из основного цикла выполнения парсинга

--- main.py
+++ main.py
@@ -19,4 +19,3 @@ if __name__ == '__main__':
     FINISH_TIME = datetime.datetime.now()
     DURATION = FINISH_TIME - START_TIME
     print(f'= DURATION: {DURATION}')
-    print(tree_root.search('Python'))
